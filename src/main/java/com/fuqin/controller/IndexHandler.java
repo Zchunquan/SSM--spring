2dba7575242a85ed3bc88032c04f44a377ad9dbd
zhaochunquan <zchunquan@126.com>
2019-07-14 00:41:18 +0800

    test2222222

--- src/main/java/com/fuqin/controller/IndexHandler.java
+++ src/main/java/com/fuqin/controller/IndexHandler.java
@@ -16,6 +16,7 @@ public class IndexHandler {
 	
 	@RequestMapping(value={"tosuccess"},method=RequestMethod.GET)
 	public String tosuccess(){
+	    22222222
 		int count = userService.selectcountbyorderno();
 		System.out.println(count);
 		return "success";
